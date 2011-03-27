51df4f259de1527fbdaa8b7d0e2f7679016e759b
blandinw <wblandin@gmail.com>
2011-03-27 20:53:34 +0200

    Symfony PR8 LOADED....

---
+++ src/Application/OrganizerBundle/Controller/OrganizerController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Application\OrganizerBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Application\CalendarBundle\Event\EventForm;
+use Symfony\Component\Form\CsrfProvider\SessionCsrfProvider;
+
+class OrganizerController extends Controller
+{
+    public function indexAction()
+    {
+        $request = $this->get('request');
+
+        //must be logged in
+
+        // get calendar from and to dates
+        $start = $request->query->get('from');
+        $range = $request->query->get('range');
+        $range = isset($range) ? $range : 7;
+        $from = new \DateTime($start);
+        $to = clone($from);
+        $to->add(new \DateInterval('P'. ($range - 1) .'D'));
+
+        // generate csrf token
+        $form = EventForm::create($this->get('form.context'), 'event');
+        $token = $form->get($form->getCsrfFieldName())->getDisplayedData();
+
+        return $this->render('OrganizerBundle:Organizer:index.twig.html',
+                             array(
+                                   'from' => $from,
+                                   'to' => $to,
+                                   'token' => $token,
+                                   ));
+    }
+}
